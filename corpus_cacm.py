f9dc03a6989cf203b36f25983953c42bc0547efc
Erwan De Kergaradec <erwan.le-jumeau-de-kergaradec@student.ecp.fr>
2018-12-10 12:47:33 +0100

    dbt index inv

--- corpus_cacm.py
+++ corpus_cacm.py
@@ -5,6 +5,7 @@
 from nltk import wordpunct_tokenize
 import os
 import re
+from functools import reduce
 
 
 def read_file(path):
@@ -63,13 +64,76 @@ def tokenisation(collection):
     return collection_token
 
 
+def remove_common_words(token, path_common):
+
+    if not os.path.exists(path_common):
+        raise FileNotFoundError("file {} doesn't exist".format(path_common))
+
+    list_stop_words = []
+    with open(path_common, 'r') as file:
+        for l in file.readlines():
+            list_stop_words.append(l[:-1])
+
+    for docid in token:
+        for sentence in token[docid]:
+            for words in sentence:
+                if words in list_stop_words:
+                    sentence.remove(words)
+
+    return token
+
+
+def nb_token(collection):
+    return reduce(lambda acc1, y: acc1 + reduce(lambda acc2, z: acc2 + len(z), collection[y], 0), collection, 0)
+
+
+def agg_terms(collection):
+    def mapper(docid):
+        dic_term = {'T': collection[docid][0] if len(collection[docid]) > 0 else [],
+                    'W': collection[docid][1] if len(collection[docid]) > 1 else [],
+                    'K': collection[docid][2] if len(collection[docid]) > 2 else []
+                    }
+        return docid, dic_term
+    map_coll = list(map(mapper, collection))
+
+    print(map_coll[2])
+
+    dic_sent = {0: 'T', 1: 'W', 2: 'K'}
+    index_inv = {}
+    for docid in collection:
+        dic_term = {'T': [],
+                    'W': [],
+                    'K': []
+                    }
+        for i, el in enumerate(collection[docid]):
+            long = len(collection[docid[i]])
+            for words in el:
+                dic_term = {'T': 1/len(collection[docid][0]) if i == 0 else 0,
+                            'W': 1/len(collection[docid][1]) if i == 1 else 0,
+                            'K': 1/len(collection[docid][2]) if i == 2 else 0,
+                            }
+                if words not in index_inv:
+                    index_inv[words] = [(docid, dic_term)]
+                else:
+                    ancienne_freq = index_inv[words][docid]['T']
+                    index_inv[words].append((docid, dic_term))
+
+    exemple = {'techniques': [(3, {'T': 0.2, 'W': 0, 'K': 0}), (16, {'T': 0.1, 'W': 0, 'K': 0})]}
+
+
 if __name__ == '__main__':
 
     path = os.path.join('cacm', 'cacm.all')
+    path_common = os.path.join('cacm', 'common_words')
     files = read_file(path)
     col_token = tokenisation(files)
-    for docid in col_token:
-        print(docid, col_token[docid])
+    col_token = remove_common_words(col_token, path_common)
 
+    agg_terms(col_token)
+
+    #for docid in col_token:
+    #    print(docid, col_token[docid])
+
+    # nombre de tokens
 
 
