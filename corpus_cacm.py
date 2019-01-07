8baed66a6636187a93221e1bf75078f26db1b892
Erwan De Kergaradec <erwan.le-jumeau-de-kergaradec@student.ecp.fr>
2019-01-07 12:30:26 +0100

    moteur de techerche fonctionnel

--- corpus_cacm.py
+++ corpus_cacm.py
@@ -6,7 +6,8 @@ from nltk import wordpunct_tokenize
 import os
 import re
 from functools import reduce
-from datetime import datetime
+from math import log, cos, sqrt
+import time
 import matplotlib.pyplot as plt
 
 
@@ -131,16 +132,28 @@ def build_index_inv(collection_tokens):
 def graphe_frequence_rang(index_inv, collection_tokens):
 
     frequences = []
-    rangs = []
-    itr = 0
     for mot in index_inv:
         frequences.append(len(index_inv[mot].keys())/len(collection_tokens))
-        rangs.append(itr) # aucune idee de ce qu'est le rang
-        itr += 1
 
+    frequences = sorted(frequences, reverse=True)
+    log_frequences = list(map(lambda el: log(el), frequences))
+    rangs = range(1, len(frequences) + 1)
+    log_rangs = list(map(lambda el: log(el), rangs))
+
+    plt.figure(figsize=(15, 6))
+    # Tracé du graphe de la fréquence en fonction du rang du token
+    plt.subplot(1, 2, 1)
     plt.xlabel("Rang du token")
     plt.ylabel("Fréquence du token")
+    plt.title("Graphe de la fréquence en fonction du rang")
     plt.plot(rangs, frequences)
+
+    # Tracé du graphe du log
+    plt.subplot(1, 2, 2)
+    plt.xlabel("Log du rang du token")
+    plt.ylabel("Log de la fréquence du token")
+    plt.title("Graphe du log de la fréquence en fonction du log du rang")
+    plt.plot(log_rangs, log_frequences)
     plt.show()
 
 
@@ -176,6 +189,99 @@ def boolean_request(mot1, op, mot2, index_inv):
     return docids_request
 
 
+def vectorization_bool(request, request_type, index_inv, collection_tokens, path_common_words):
+
+    if request_type not in ('T', 'W', 'K'):
+        raise ValueError("type should be in ('T', 'W', 'K'), is {}".format(request_type))
+    col = {0: [request]}
+    req_term = tokenisation(col, path_common_words)[0][0]
+
+    q = []
+    d = [[0 for i in range(len(index_inv))] for i in range(len(collection_tokens))]
+    i = 0
+    for term in index_inv:
+        if term in req_term:
+            q.append(1)
+        else:
+            q.append(0)
+
+        liste = [key for key in index_inv[term] if index_inv[term][key][request_type] != 0]
+        for k in liste:
+            d[k-1][i] = 1
+        i += 1
+
+    return q, d
+
+
+def vectorization_tfidf(request, request_type, index_inv, collection_tokens, path_common_words):
+
+    if request_type not in ('T', 'W', 'K'):
+        raise ValueError("type should be in ('T', 'W', 'K'), is {}".format(request_type))
+    col = {0: [request]}
+    req_term = tokenisation(col, path_common_words)[0][0]
+
+    nb_doc = len(collection_tokens)
+    q = []
+    d = [[0 for i in range(len(index_inv))] for i in range(nb_doc)]
+    i = 0
+
+    map_request_type = {'T': 0, 'W': 1, 'K': 2}
+
+    for term in index_inv:
+        liste = [key for key in index_inv[term] if index_inv[term][key][request_type] != 0]
+        idf = log(nb_doc / len(liste), 10) if len(liste) != 0 else 0
+
+        tf_q = req_term.count(term)
+        if tf_q != 0:
+            q.append((1 + log(tf_q, 10)) * idf)
+        else:
+            q.append(0)
+
+        for k in liste:
+            nb_mots_doc = len(collection_tokens[k][map_request_type[request_type]])
+            tf = nb_mots_doc * index_inv[term][k][request_type]
+            d[k-1][i] = (1 + log(tf, 10)) * idf
+        i += 1
+
+    return q, d
+
+
+def compute_similarity(vec_request, vec_collections):
+
+    def dotproduct(v1, v2):
+        return sum((a * b) for a, b in zip(v1, v2))
+
+    def length(v):
+        return sqrt(dotproduct(v, v))
+
+    def sim(v1, v2):
+        return dotproduct(v1, v2) / (length(v1) * length(v2))
+
+    simil = []
+    for i in range(len(vec_collections)):
+        simil.append(sim(vec_collections[i], vec_request))
+
+    doc_similarity = list(map(lambda el: (el[0] + 1, el[1]), enumerate(simil)))
+    doc_similarity = sorted(doc_similarity, key=lambda el: el[1], reverse=True)
+    doc_similarity = list(map(lambda el: el[0], doc_similarity))[:10]
+
+    return doc_similarity
+
+
+def vector_request_bool(request, request_type, index_inv, path_common_words, collection_tokens, ponderation):
+
+    if ponderation == 'bool':
+        vec_request, vec_collection = vectorization_bool(request, request_type, index_inv, collection_tokens, path_common_words)
+    elif ponderation == 'tfidf':
+        vec_request, vec_collection = vectorization_tfidf(request, request_type, index_inv, collection_tokens, path_common_words)
+    else:
+        raise ValueError("ponderation is not correct")
+
+    doc_similarity = compute_similarity(vec_request, vec_collection)
+
+    return doc_similarity
+
+
 if __name__ == '__main__':
 
     path = os.path.join('cacm', 'cacm.all')
@@ -189,10 +295,15 @@ if __name__ == '__main__':
     # print(nb_token(collection_tokens))
     # >>> 118931
     # print(collection_tokens[103])
+    # >>> [['cope', 'console'], ['each', year', ..]] # pas de mots clés
     index_inv = build_index_inv(collection_tokens)
     # print(len(index_inv))
-    # >>> 9818
-    graphe_frequence_rang(index_inv, collection_tokens)
-    result = boolean_request("language", "AND", "system", index_inv)
-    print(result)
+    # >>> 9723
+    # print(index_inv['language'])
+    # >>> {1: {'T': 0.2, 'W': 0, 'K': 0}, 82: {'T': 0.16666666666666666, 'W': 0.07692307692307693, 'K': 0}, ..}
+
+    # graphe_frequence_rang(index_inv, collection_tokens)
+    # result = boolean_request("language", "AND", "system", index_inv)
+    doclist = vector_request_bool('what is the language of France ?', 'T', index_inv, path_common_words, collection_tokens, 'tfidf')
+    print(doclist)
 
