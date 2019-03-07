751794a6368a2f855a73359d6bd30f2429113342
charlesdlrsa <charlesdlrsa@hotmail.fr>
2019-03-07 16:34:26 +0100

    derniers changements

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 
 from code.file_manager import read_file, read_answers
 from code.nlp_processing import tokenisation, nb_tokens
-from code.browser import build_index_inv, display_graph_freq_rank
+from code.browser import build_index_inv, display_graph_freq_rank, boolean_request
 
 if __name__ == '__main__':
 
@@ -34,15 +34,16 @@ if __name__ == '__main__':
     index_inv = build_index_inv(collection_tokens)
     print("Taille de notre vocabulaire : ", len(index_inv), "\n")
     print("Index inversé du mot 'variable' : \n", index_inv['variabl'], "\n")
-    print("Fermez la fenêtre du graphe pour que le code continue.")
+    print("Fermez la fenêtre du graphe pour que le code continue. \n")
     display_graph_freq_rank(index_inv, collection_tokens)
 
+    # Modèle de recherche booléen
+    print("Résultat de recherche booléenne sur la requête suivante : 'language', 'AND', 'Implementation' : \n")
+    print(boolean_request("language", "AND", "Implementation", index_inv))
 
     # Création d'une requête de test
     query = {0: 'What articles exist which deal with TSS (Time Sharing System), an operating system for IBM computers?'}
     query_tokens = tokenisation(query, path_common_words, stemming=True)
-
-    # print(boolean_request("language", "AND", "Implementation", index_inv))
 
     # Vectorisation booléenne
     # vectorizer = BooleanVectorizer()
